cdaaba1c62219822537c27fa06bfb8bd33b199c4
weicheng2138 <weicheng2138@gmail.com>
2024-05-03 17:05:11 +0800

    feat(middleware): add locale for app router

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,6 @@
-import { isFullBlock, isFullPage } from "@notionhq/client";
 import { notion } from "@/lib/notion";
-export default async function Home() {
-  console.log(process.env.NOTION_TOKEN);
+export default async function Home({ params }: { params: { lang: string } }) {
+  console.log(params.lang);
   if (!process.env.NOTION_TOKEN || !process.env.NOTION_DATABASE_ID) {
     return <div>Notion token or database id is missing</div>;
   }
@@ -9,15 +8,8 @@ export default async function Home() {
     database_id: process.env.NOTION_DATABASE_ID,
   });
 
-  console.log(isFullBlock(pages.results[0]));
-  console.log(isFullPage(pages.results[0]));
-  console.log(pages.results.map((page) => page.id));
-
   const pageIds = pages.results.map((page) => page.id);
   const blocks = await notion.blocks.children.list({ block_id: pageIds[0] });
-  console.log(isFullBlock(blocks.results[0]));
-  console.log(isFullPage(blocks.results[0]));
-  console.log(blocks.results[2]);
   // if ("type" in blocks.results[0] && "paragraph" in blocks.results[0]) {
   //   console.log(blocks.results[0].paragraph.rich_text[0].plain_text);
   // }
@@ -27,7 +19,6 @@ export default async function Home() {
     blocks.results[2].callout.icon &&
     blocks.results[2].callout.icon.type === "emoji"
   ) {
-    console.log(blocks.results[2].callout.icon.emoji);
     return (
       <div>
         <h1>{blocks.results[2].callout.icon.emoji}</h1>
---
+++ middleware.ts
@@ -0,0 +1,48 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const locales = ["en-US", "zh-TW"];
+const defaultLocale = "en-US";
+
+/**
+ * Get the locale from the request headers
+ * If no locale is found, return the default locale
+ * @param {NextRequest} request
+ * @returns {string} The locale
+ *
+ */
+function getLocale(request: NextRequest): string {
+  let headers = {
+    "accept-language": request.headers.get("accept-language") || "",
+  };
+  let languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+/**
+ * Middleware to redirect to the correct locale
+ * @param {NextRequest} request
+ */
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+  if (pathnameHasLocale) return;
+
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  return NextResponse.redirect(request.nextUrl);
+}
+
+/**
+ * Configuration for the middleware
+ * @type {import("next/types").NextConfig}
+ */
+export const config: import("next/types").NextConfig = {
+  // Matcher ignoring `/_next/` and `/api/`
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
